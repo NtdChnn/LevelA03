96a3f4f9d07395557f3700384f0937c6c82b4646
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-11-17 10:37:39 +0700

    finish

--- LevelA03/6301480.cpp
+++ LevelA03/6301480.cpp
@@ -2,7 +2,14 @@
 int findV(int A, int T, int U)
 {
 	int V;
-	V = U + (A*T);
+	if (T < 0 || U < 0)
+	{
+		V = -1;
+	}
+	else 
+	{
+		V = U + (A * T);
+	}
 	return V;
 }
 int main()
@@ -32,8 +39,14 @@ int main()
 	object01.V = findV(object01.A, object01.T, object01.U);
 	object02.V = findV(object02.A, object02.T, object02.U);
 
-	printf("\nCurrent speed of object 1 is %d\n", object01.V);
-	printf("Current speed of object 2 is %d", object02.V);
+	if (object01.V == -1)
+	{
+		printf("\n OBJECT 1 IS E R R O R ! ! \n");
+	} else printf("\nCurrent speed of object 1 is %d\n", object01.V);
+	if (object02.V == -1)
+	{
+		printf(" OBJECT 2 IS E R R O R ! ! ");
+	} else printf("Current speed of object 2 is %d", object02.V);
 
 	return 0;
 }
